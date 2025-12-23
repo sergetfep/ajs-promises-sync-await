37edc9a27c71dd1ab8b75ca6e02637d581982cd0
Serge <nsecuritysg@nxt.ru>
2025-12-24 00:41:44 +0300

    first commit

---
+++ src/GameSaving.js
@@ -0,0 +1,5 @@
+export default class GameSaving {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+}
---
+++ src/GameSavingLoader.js
@@ -0,0 +1,12 @@
+import read from './reader';
+import json from './parser';
+import GameSaving from './GameSaving';
+
+export default class GameSavingLoader {
+  static async load() {
+    const data = await read();
+    const value = await json(data);
+    const parsed = JSON.parse(value);
+    return new GameSaving(parsed);
+  }
+}
